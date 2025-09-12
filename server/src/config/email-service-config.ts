91ff3595fd30192ce0b0c84e5d50940d3ceb498e
ebbryan <earlbryanburaay@gmail.com>
2025-09-12 22:37:02 +0800

    fixed leaked values

--- server/src/config/email-service-config.ts
+++ server/src/config/email-service-config.ts
@@ -1,10 +1,9 @@
+require("dotenv").config({ path: [".env.local", ".env"] });
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: process.env.BREVO_SMTP_HOST as string,
-  port: process.env.BREVO_SMTP_PORT
-    ? parseInt(process.env.BREVO_SMTP_PORT)
-    : undefined,
+  port: 587,
   secure: false,
   auth: {
     user: process.env.BREVO_SMTP_USER as string,
